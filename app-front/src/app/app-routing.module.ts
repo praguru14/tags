75bb96017a2d1d8fbcb248aa4a2592b6e4134064
Prafull Gururani <praguru14@gmail.com>
2024-03-05 13:37:36 +0530

    fixed frontend

--- app-front/src/app/app-routing.module.ts
+++ app-front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './header/home/home.component';
+import { AboutComponent } from './header/about/about.component';
+import { ContactUsComponent } from './header/contact-us/contact-us.component';
+import { ProductInfoComponent } from './header/product-info/product-info.component';
+import { SetupComponent } from './header/setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'product-info', component: ProductInfoComponent },
+  { path: 'setup', component: SetupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-front/src/app/app.module.ts
+++ app-front/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './header/home/home.component';
+import { AboutComponent } from './header/about/about.component';
+import { ContactUsComponent } from './header/contact-us/contact-us.component';
+import { ProductInfoComponent } from './header/product-info/product-info.component';
+import { SetupComponent } from './header/setup/setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent,
+    ProductInfoComponent,
+    SetupComponent
   ],
   imports: [
     BrowserModule,
