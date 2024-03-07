ac4ffc5cbd8680eb75a4c393e3d46e3a54ee4fde
Prafull Gururani <praguru14@gmail.com>
2024-03-07 17:22:27 +0530

    add new fields

--- backend/src/main/java/com/tag/backend/model/UserModel.java
+++ backend/src/main/java/com/tag/backend/model/UserModel.java
@@ -3,25 +3,98 @@ package com.tag.backend.model;
 import com.tag.backend.entity.User;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Getter
 @Setter
-public class UserModel
-{
-	private long id;
-	private String email;
-	private String phone;
-	private String firstName;
-	private String lastName;
-	private String bloodGroup;
+public class UserModel {
+    private long id;
 
-	public User convertToEntity(User user)
-	{
-		user.setEmail(this.email);
-		user.setPhone(this.phone);
-		user.setFirstName(this.firstName);
-		user.setLastName(this.lastName);
-		user.setBloodGroup(this.bloodGroup);
-		return user;
-	}
+    private String email;
+
+    private String phone;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String bloodGroup;
+
+    private Date dob;
+
+    private String gender;
+
+    private String hairColor;
+
+    private String eyeColor;
+
+    private BigDecimal height;
+
+    private BigDecimal weight;
+
+    private String identificationMark;
+
+    private String ecName;
+
+    private String ecRelationship;
+
+    private String ecNumber;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private String country;
+
+    private String allergyName;
+
+    private String allergyNotes;
+
+    private String medicineName;
+
+    private String medicineNotes;
+
+    private BigDecimal medicineDosage;
+
+    private String medicineDosageUnit;
+
+    private Integer medicineFrequency;
+
+    private String medicineFrequencyType;
+
+    private String insuranceName;
+
+    private String insuranceNotes;
+
+    private String medicalConditionName;
+
+    private String medicalConditionNotes;
+
+    private Date registrationDttm;
+
+    private Boolean isVerified;
+
+    private Boolean isActive;
+
+    private String profilePhoto;
+
+    private Date lastLoginDttm;
+
+    private Boolean isBanned;
+
+    private String middleName;
+
+    private String userType;
+
+    public User convertToEntity(User user) {
+        BeanUtils.copyProperties(this, user);
+        return user;
+    }
 }
--- mysql/login_user_creation.sql
+++ mysql/login_user_creation.sql
@@ -18,3 +18,42 @@ CREATE TABLE login (
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
+ALTER TABLE user
+ADD COLUMN dob DATE,
+ADD COLUMN gender VARCHAR(1),
+ADD COLUMN hair_color VARCHAR(20),
+ADD COLUMN eye_color VARCHAR(20),
+ADD COLUMN height DECIMAL(5,2),
+ADD COLUMN weight DECIMAL(5,2),
+ADD COLUMN identification_mark TEXT,
+ADD COLUMN ec_name VARCHAR(100),
+ADD COLUMN ec_relationship VARCHAR(50),
+ADD COLUMN ec_number VARCHAR(20),
+ADD COLUMN address VARCHAR(255),
+ADD COLUMN city VARCHAR(100),
+ADD COLUMN state VARCHAR(100),
+ADD COLUMN zip VARCHAR(20),
+ADD COLUMN country VARCHAR(100),
+ADD COLUMN allergy_name VARCHAR(100),
+ADD COLUMN allergy_notes TEXT,
+ADD COLUMN medicine_name VARCHAR(100),
+ADD COLUMN medicine_notes TEXT,
+ADD COLUMN medicine_dosage DECIMAL(5,2),
+ADD COLUMN medicine_dosage_unit VARCHAR(20),
+ADD COLUMN medicine_frequency INT,
+ADD COLUMN medicine_frequency_type VARCHAR(20),
+ADD COLUMN insurance_name VARCHAR(100),
+ADD COLUMN insurance_notes TEXT,
+ADD COLUMN medical_condition_name VARCHAR(100),
+ADD COLUMN medical_condition_notes TEXT,
+ADD COLUMN registration_dttm DATE,
+ADD COLUMN is_verified BOOLEAN,
+ADD COLUMN is_active BOOLEAN,
+ADD COLUMN profile_photo VARCHAR(255),
+ADD COLUMN last_login_dttm DATE,
+ADD COLUMN is_banned BOOLEAN,
+ADD COLUMN middle_name VARCHAR(255),
+ADD COLUMN user_type VARCHAR(255);
+
+select * from user;
+delete from user where id >35;
