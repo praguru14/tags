623d38d1c14e863a9b05b996c70cb39be360c948
Prafull Gururani <praguru14@gmail.com>
2024-03-07 21:49:26 +0530

    Ip test

--- backend/src/main/java/com/tag/backend/controllers/WhiteLabelError.java
+++ backend/src/main/java/com/tag/backend/controllers/WhiteLabelError.java
@@ -1,8 +1,13 @@
 package com.tag.backend.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class WhiteLabelError implements ErrorController {
@@ -10,5 +15,41 @@ public class WhiteLabelError implements ErrorController {
     @RequestMapping("/error")
     public String handleError() {
         return "error";
+    }
+
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    @GetMapping("/")
+    @ResponseBody
+    public static String getClientIpAddressIfServletRequestExist() {
+
+        if (RequestContextHolder.getRequestAttributes() == null) {
+            return "0.0.0.0";
+        }
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return "Ip is :" + ip;
+            }
+        }
+        String ip = request.getRemoteAddr();
+        if (ip.equals("0:0:0:0:0:0:0:1")) ip = "127.0.0.1";
+        return "ip is :" + ip;
     }
 }
