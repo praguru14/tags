648b34c43c81a2380a5cbeb1aa8fdfd7b27efe54
Prafull Gururani <praguru14@gmail.com>
2024-03-17 22:21:30 +0530

    update password

--- backend/src/main/java/com/tag/backend/services/LoginService.java
+++ backend/src/main/java/com/tag/backend/services/LoginService.java
@@ -86,4 +86,15 @@ public class LoginService {
                 .bloodGroup(loginModel.getBloodGroup())
                 .build();
     }
+
+    public DataMessage updatePassword(String email, String currentPassword, String newPassword) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, currentPassword));
+        Login user = loginRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setPassword(passwordEncoder.encode(newPassword));
+        loginRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return new DataMessage(HttpStatus.OK, null, "Password updated successfully", jwtToken);
+    }
+
+
 }
