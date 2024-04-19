524030775b605d8de46e60c68b035e1b6fc0e8ce
Prafull Gururani <praguru14@gmail.com>
2024-04-19 12:35:41 +0530

    added -params in pom.xml

--- app-front/src/app/model/baseUrl.model.ts
+++ app-front/src/app/model/baseUrl.model.ts
@@ -1,5 +1,8 @@
 
 export class BaseUrl{
-    public static baseUrl = 'http://localhost:8400/tag/';
-    public static register = 'http://localhost:8400/';
+    // public static baseUrl = 'http://localhost:8400/tag/';
+    // public static register = 'http://localhost:8400/';
+    public static baseUrl = 'https://tags-priv.onrender.com/tag/';
+    public static register = 'https://tags-priv.onrender.com/';
+
 }
