0b5434b9bf222027800b6e08c7256f4feec90f51
Prafull Gururani <praguru14@gmail.com>
2024-04-19 21:05:21 +0530

    localhost env

--- app-front/src/app/model/baseUrl.model.ts
+++ app-front/src/app/model/baseUrl.model.ts
@@ -1,8 +1,8 @@
 
 export class BaseUrl{
-    // public static baseUrl = 'http://localhost:8400/tag/';
-    // public static register = 'http://localhost:8400/';
-    public static baseUrl = 'https://tags-priv.onrender.com/tag/';
-    public static register = 'https://tags-priv.onrender.com/';
+    public static baseUrl = 'http://localhost:8400/tag/';
+    public static register = 'http://localhost:8400/';
+    // public static baseUrl = 'https://tags-priv.onrender.com/tag/';
+    // public static register = 'https://tags-priv.onrender.com/';
 
 }
