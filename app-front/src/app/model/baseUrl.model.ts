10f807c9015455b2b710fccc607b37b3584371c2
Prafull Gururani <praguru14@gmail.com>
2024-08-30 08:08:41 +0530

    changed origins

--- app-front/src/app/model/baseUrl.model.ts
+++ app-front/src/app/model/baseUrl.model.ts
@@ -1,8 +1,10 @@
 
 export class BaseUrl{
-    public static baseUrl = 'http://localhost:8400/tag/';
-    public static register = 'http://localhost:8400/';
+    // public static baseUrl = 'http://localhost:8400/tag/';
+    // public static register = 'http://localhost:8400/';
     // public static baseUrl = 'https://tags-priv.onrender.com/tag/';
     // public static register = 'https://tags-priv.onrender.com/';
-
+    public static baseUrl = 'https://tags-c2k9.onrender.com/tag/';
+    public static register = 'https://tags-c2k9.onrender.com/';
+//https://tags-c2k9.onrender.com
 }
